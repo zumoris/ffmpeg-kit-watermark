953cd6df544019526e947d657bfaa07c250a96b6
Sir Matter <dtseyko@gmail.com>
2024-08-25 14:10:15 +0300

    Update android-build-full.sh

--- android-build-full.sh
+++ android-build-full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ANDROID_SDK_ROOT="/Users/odmen/Library/Android/sdk";
-export ANDROID_NDK_ROOT="/Users/odmen/Library/Android/sdk/ndk/25.1.8937393";
+export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk";
+export ANDROID_NDK_ROOT="$HOME/Library/Android/sdk/ndk/25.1.8937393";
 
 ./android.sh --enable-x264 --enable-gpl --disable-x86 --disable-x86-64  --disable-arm-v7a-neon
